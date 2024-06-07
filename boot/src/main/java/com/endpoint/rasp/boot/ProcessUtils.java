dcb17602cb5519f985e259b125f2940ae7a97f86
f <2825097536@qq.com>
2024-06-07 19:25:47 +0800

    init: 初始化模块

---
+++ boot/src/main/java/com/endpoint/rasp/boot/ProcessUtils.java
@@ -0,0 +1,214 @@
+package com.endpoint.rasp.boot;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author hengyunabc 2018-11-06
+ */
+public class ProcessUtils {
+    private static String FOUND_JAVA_HOME = null;
+
+
+    /**
+     * <pre>
+     * 1. Try to find java home from System Property java.home
+     * 2. If jdk > 8, FOUND_JAVA_HOME set to java.home
+     * 3. If jdk <= 8, try to find tools.jar under java.home
+     * 4. If tools.jar do not exists under java.home, try to find System env JAVA_HOME
+     * 5. If jdk <= 8 and tools.jar do not exists under JAVA_HOME, throw IllegalArgumentException
+     * </pre>
+     *
+     * @return
+     */
+    public static String findJavaHome() {
+        if (FOUND_JAVA_HOME != null) {
+            return FOUND_JAVA_HOME;
+        }
+
+        String javaHome = System.getProperty("java.home");
+
+        if (JavaVersionUtils.isLessThanJava9()) {
+            File toolsJar = new File(javaHome, "lib/tools.jar");
+            if (!toolsJar.exists()) {
+                toolsJar = new File(javaHome, "../lib/tools.jar");
+            }
+            if (!toolsJar.exists()) {
+                // maybe jre
+                toolsJar = new File(javaHome, "../../lib/tools.jar");
+            }
+
+            if (toolsJar.exists()) {
+                FOUND_JAVA_HOME = javaHome;
+                return FOUND_JAVA_HOME;
+            }
+
+            if (!toolsJar.exists()) {
+                System.out.println(("Can not find tools.jar under java.home: " + javaHome));
+                String javaHomeEnv = System.getenv("JAVA_HOME");
+                if (javaHomeEnv != null && !javaHomeEnv.isEmpty()) {
+                    System.out.println(("Try to find tools.jar in System Env JAVA_HOME: " + javaHomeEnv));
+                    // $JAVA_HOME/lib/tools.jar
+                    toolsJar = new File(javaHomeEnv, "lib/tools.jar");
+                    if (!toolsJar.exists()) {
+                        // maybe jre
+                        toolsJar = new File(javaHomeEnv, "../lib/tools.jar");
+                    }
+                }
+
+                if (toolsJar.exists()) {
+                    System.out.println(("Found java home from System Env JAVA_HOME: " + javaHomeEnv));
+                    FOUND_JAVA_HOME = javaHomeEnv;
+                    return FOUND_JAVA_HOME;
+                }
+
+                throw new IllegalArgumentException("Can not find tools.jar under java home: " + javaHome
+                        + ", please try to start arthas-boot with full path java. Such as /opt/jdk/bin/java -jar arthas-boot.jar");
+            }
+        } else {
+            FOUND_JAVA_HOME = javaHome;
+        }
+        return FOUND_JAVA_HOME;
+    }
+
+    public static void startArthasCore(long targetPid, List<String> commands) {
+        // find java/java.exe, then try to find tools.jar
+        String javaHome = findJavaHome();
+
+        // find java/java.exe
+        File javaPath = findJava(javaHome);
+        if (javaPath == null) {
+            throw new IllegalArgumentException(
+                    "Can not find java/java.exe executable file under java home: " + javaHome);
+        }
+
+        File toolsJar = findToolsJar(javaHome);
+
+        if (JavaVersionUtils.isLessThanJava9()) {
+            if (toolsJar == null || !toolsJar.exists()) {
+                throw new IllegalArgumentException("Can not find tools.jar under java home: " + javaHome);
+            }
+        }
+
+        List<String> command = new ArrayList<>();
+        command.add(javaPath.getAbsolutePath());
+
+        if (toolsJar != null && toolsJar.exists()) {
+            commands.add("-Xbootclasspath/a:" + toolsJar.getAbsolutePath());
+        }
+        // "${JAVA_HOME}"/bin/java \
+        // ${opts} \
+        // -jar "${arthas_lib_dir}/arthas-core.jar" \
+        // -pid ${TARGET_PID} \
+        // -target-ip ${TARGET_IP} \
+        // -telnet-port ${TELNET_PORT} \
+        // -http-port ${HTTP_PORT} \
+        // -core "${arthas_lib_dir}/arthas-core.jar" \
+        // -agent "${arthas_lib_dir}/arthas-agent.jar"
+        command.addAll(commands);
+        ProcessBuilder pb = new ProcessBuilder(command);
+        // https://github.com/alibaba/arthas/issues/2166
+       pb.environment().put("JAVA_TOOL_OPTIONS", "");
+        try {
+            final Process proc = pb.start();
+            Thread redirectStdout = new Thread(() -> {
+                InputStream inputStream = proc.getInputStream();
+                try {
+                    IOUtils.copy(inputStream, System.out);
+                } catch (IOException e) {
+                    IOUtils.close(inputStream);
+                }
+
+            });
+
+            Thread redirectStderr = new Thread(() -> {
+                InputStream inputStream = proc.getErrorStream();
+                try {
+                    IOUtils.copy(inputStream, System.err);
+                } catch (IOException e) {
+                    IOUtils.close(inputStream);
+                }
+
+            });
+            redirectStdout.start();
+            redirectStderr.start();
+            redirectStdout.join();
+            redirectStderr.join();
+
+            int exitValue = proc.exitValue();
+            if (exitValue != 0) {
+                //AnsiLog.error("attach fail, targetPid: " + targetPid);
+                System.exit(1);
+            }
+        } catch (Throwable e) {
+            // ignore
+        }
+    }
+
+
+    private static File findJava(String javaHome) {
+        String[] paths = {"bin/java", "bin/java.exe", "../bin/java", "../bin/java.exe"};
+
+        List<File> javaList = new ArrayList<File>();
+        for (String path : paths) {
+            File javaFile = new File(javaHome, path);
+            if (javaFile.exists()) {
+                /// AnsiLog.debug("Found java: " + javaFile.getAbsolutePath());
+                javaList.add(javaFile);
+            }
+        }
+
+        if (javaList.isEmpty()) {
+            // AnsiLog.debug("Can not find java/java.exe under current java home: " + javaHome);
+            return null;
+        }
+
+        // find the shortest path, jre path longer than jdk path
+        if (javaList.size() > 1) {
+            Collections.sort(javaList, new Comparator<File>() {
+                @Override
+                public int compare(File file1, File file2) {
+                    try {
+                        return file1.getCanonicalPath().length() - file2.getCanonicalPath().length();
+                    } catch (IOException e) {
+                        // ignore
+                    }
+                    return -1;
+                }
+            });
+        }
+        return javaList.get(0);
+    }
+
+    private static File findToolsJar(String javaHome) {
+        if (JavaVersionUtils.isGreaterThanJava8()) {
+            return null;
+        }
+
+        File toolsJar = new File(javaHome, "lib/tools.jar");
+        if (!toolsJar.exists()) {
+            toolsJar = new File(javaHome, "../lib/tools.jar");
+        }
+        if (!toolsJar.exists()) {
+            // maybe jre
+            toolsJar = new File(javaHome, "../../lib/tools.jar");
+        }
+
+        if (!toolsJar.exists()) {
+            throw new IllegalArgumentException("Can not find tools.jar under java home: " + javaHome);
+        }
+
+        // AnsiLog.debug("Found tools.jar: " + toolsJar.getAbsolutePath());
+        return toolsJar;
+    }
+
+
+    private static boolean isJpsProcess(String mainClassName) {
+        return "sun.tools.jps.Jps".equals(mainClassName) || "jdk.jcmd/sun.tools.jps.Jps".equals(mainClassName);
+    }
+}
---
+++ core/src/main/java/com/endpoint/rasp/engine/Core.java
@@ -0,0 +1,14 @@
+package com.endpoint.rasp.engine;
+
+/**
+ * @author: feiwoscun
+ * @date: 2024/6/7
+ * @email: 2825097536@qq.com
+ * @description:
+ */
+public class Core {
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
+}
