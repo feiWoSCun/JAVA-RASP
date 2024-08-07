c003f13451a57ed621df399c993a52c22f8b8d23
f <2825097536@qq.com>
2024-08-07 09:47:42 +0800

    update：IOUtils copy逻辑优化

--- boot/src/main/java/com/endpoint/rasp/boot/IOUtils.java
+++ boot/src/main/java/com/endpoint/rasp/boot/IOUtils.java
@@ -18,11 +18,7 @@ public class IOUtils {
 
     public static String toString(InputStream inputStream) throws IOException {
         ByteArrayOutputStream result = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = inputStream.read(buffer)) != -1) {
-            result.write(buffer, 0, length);
-        }
+        copy(inputStream, result);
         return result.toString("UTF-8");
     }
 
